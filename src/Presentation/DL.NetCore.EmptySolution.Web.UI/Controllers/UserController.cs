c41c4a6a641a8dfb4a5ff14bd2c26c089557f993
davidliang2008 <david.liang@officeally.com>
2020-07-31 11:29:33 -0700

    Add a demo to show how to display asp-validaiton-summary on field changes.

---
+++ src/Presentation/DL.NetCore.EmptySolution.Web.UI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DL.NetCore.EmptySolution.Web.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Invite()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Presentation/DL.NetCore.EmptySolution.Web.UI/Models/User/InviteUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DL.NetCore.EmptySolution.Web.UI.Models.User
+{
+    public class InviteUserViewModel
+    {
+        [DisplayName("Email Address")]
+        [Required(ErrorMessage = "Please provide the invitee's Email Address")]
+        [EmailAddress(ErrorMessage = "Please provide a valid email address")]
+        [StringLength(254, ErrorMessage = "Maximum email address length exceeded")]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Display Name")]
+        [Required(ErrorMessage = "Please provide the invitee's display name")]
+        public string DisplayName { get; set; }
+    }
+}
